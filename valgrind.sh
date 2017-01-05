7d9e5e95fed4475e169acb9f7a1c8ce0af6aa98f
slaugh <sebszczec@wp.pl>
2017-01-05 14:15:19 +0100

    script for running valgrind in Jenkins added

--- valgrind.sh
+++ valgrind.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+DELAY=2
+
+echo "Starting Valgrind"
+valgrind --leak-check=full --xml=yes --xml-file=valgrind_result.xml ./hub &
+
+echo "Giving ${DELAY} seconds for app to work"
+sleep ${DELAY}
+
+PID=`cat hub.pid`
+echo "Killing hub PID: ${PID}"
+kill $PID
