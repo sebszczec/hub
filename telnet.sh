81e9408abae87f356143fc19745b0a70ad162a40
slaugh <sebszczec@wp.pl>
2017-02-18 23:26:35 +0100

    changes in telnet script

--- telnet.sh
+++ telnet.sh
@@ -2,6 +2,11 @@
 set timeout 10
 spawn telnet localhost 1235
 expect "Welcome"
-send "Test message1\n\r"
-send "Test message2\n\r"
-sleep 1
+send -- "Test message1\r"
+sleep 0.1
+send -- "Test message2\r"
+sleep 0.1
+send -- "\35\r"
+expect "telnet>"
+send -- "close\r" 
+
