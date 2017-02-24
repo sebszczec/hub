dfc933aedbb09ba5ef2f5a1530ea72ca6d1434c7
slaugh <sebszczec@wp.pl>
2017-02-25 00:15:28 +0100

    Valgrind issue fixed

--- src/configuration_manager.cpp
+++ src/configuration_manager.cpp
@@ -75,8 +75,6 @@ bool CM::LoadResources()
     string memoryDumpName = this->_configFile.lookup("memory_dump_name");
     this->_resources[CMV::MemoryDumpName] = new StringResource(memoryDumpName);
 
-    this->_configFile.~Config();
-
     return true;
 }
 
