71f5720fe3bdb655aeb882152cd71b6e91b6a60f
slaugh <sebszczec@wp.pl>
2017-03-11 18:38:11 +0100

    new files added to testing environemt

---
+++ testing/lib/test.py
@@ -0,0 +1,7 @@
+class Test(object):
+    def __init__(self, name):
+        self.name = name
+
+    def Run(self):
+        return True
+        
---
+++ testing/telnet_test.py
@@ -0,0 +1,5 @@
+from lib import Telnet, Test
+
+class TelnetTest(Test):
+    def __init__(self, name):
+        Test.__init__(name)
