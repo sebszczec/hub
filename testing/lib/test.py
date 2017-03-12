6ed847e706589d33f1aebbaf0283e872715bd0e4
slaugh <sebszczec@wp.pl>
2017-03-12 13:53:03 +0100

    small refactoring in functional tests

--- testing/lib/test.py
+++ testing/lib/test.py
@@ -4,7 +4,13 @@ class Test(object):
     def __init__(self, name):
         self.name = name
 
+    def setup(self):
+        return True
+
+    def teardown(self):
+        return True
+
     def run(self):
-        #print "Starting test: " + self.name
+        print "Starting test: " + self.name
         return True
         
--- testing/telnet_test.py
+++ testing/telnet_test.py
@@ -2,13 +2,10 @@ from lib.test import Test
 from lib.telnet import Telnet
 
 class TelnetTest(Test):
-    def __init__(self, name):
-        Test.__init__(self, name)
-        self.connection = Telnet("localhost", 1235, 0.1)
-
     def run(self):
         Test.run(self)
-        self.connection.connect()
-        result = self.connection.expect("Welcome")
-        self.connection.close()
+        connection = Telnet("localhost", 1235, 0.1)
+        connection.connect()
+        result = connection.expect("Welcome")
+        connection.close()
         return result
