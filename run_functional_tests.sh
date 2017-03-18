cd6916d5b324f30f995cdf11b849b301fbed3dc5
slaugh <sebszczec@wp.pl>
2017-03-18 17:11:00 +0100

    changes in FT bash script

--- run_functional_tests.sh
+++ run_functional_tests.sh
@@ -1,5 +1,59 @@
 #!/bin/bash
 
+function printHelp {
+    echo "Functional Tests Framework"
+    echo "-h|--help - prints help"
+    echo "-l|--testlist - prints list of available tests"
+    echo "-r|--random SEED - runs tests in random order with SEED"
+    echo "-t|--test test_name - runs test_name"
+}
+
+while [[ $# -eq 1 ]]
+do
+key="$1"
+
+case $key in
+    -l|--testlist)
+    ;;
+    -h|--help)
+    printHelp
+    ;;
+    *)
+    echo "Unknown argument $key or missing value"
+    exit 1
+    ;;
+esac
+exit 0
+done
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -t|--test)
+    TEST="$2"
+    shift # past argument
+    ;;
+    -r|--random)
+    SEED="$2"
+    shift # past argument
+    ;;
+    -l|--testlist)
+    shift # past argument
+    ;;
+    -h|--help)
+    printHelp
+    exit 0
+    ;;
+    *)
+    echo "Unknown argument $key"
+    exit 1
+    ;;
+esac
+shift # past argument or value
+done
+
 DELAY=2
 
 echo "Starting Valgrind"
