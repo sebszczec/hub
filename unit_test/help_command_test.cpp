3e2c470ec751e8f141733328650b51d14b81d96a
sebszczec@wp.pl <sebszczec@wp.pl>
2017-09-23 12:17:31 +0200

    unit test for .help command added

---
+++ unit_test/help_command_test.cpp
@@ -0,0 +1,1 @@
+#include "help_command_test.hpp"
---
+++ unit_test/help_command_test.hpp
@@ -0,0 +1,38 @@
+#ifndef __HELP_COMMAND_TEST
+#define __HELP_COMMAND_TEST
+
+#include <gtest/gtest.h>
+#include "help_command.hpp"
+
+class HelpCommandTest : public testing::Test
+{
+public:
+    HelpCommand * sut;
+public:
+    HelpCommandTest()
+    {
+        this->sut = new HelpCommand();
+    }
+
+    ~HelpCommandTest()
+    {
+        delete this->sut;
+    }
+};
+
+TEST_F(HelpCommandTest, Register)
+{
+    string expect = ".help";
+    auto result = sut->Register();
+    EXPECT_STREQ(expect.c_str(), result.c_str());
+}
+
+TEST_F(HelpCommandTest, PrintHelp)
+{
+    string expect = "Shows help info about command, i.e. .help <command>";
+    auto result = sut->PrintHelp();
+    EXPECT_STREQ(expect.c_str(), result.c_str());
+}
+
+
+#endif
