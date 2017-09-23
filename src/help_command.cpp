cd39afdd253760d9edd9125d99e14c93aafd39d5
sebszczec@wp.pl <sebszczec@wp.pl>
2017-09-23 12:48:43 +0200

    new UT for .help command added

--- src/help_command.cpp
+++ src/help_command.cpp
@@ -10,12 +10,20 @@ string HelpCommand::PrintHelp()
     return "Shows help info about command, i.e. .help <command>";
 }
 
-bool HelpCommand::Execute(const CommandArgument &)
+bool HelpCommand::Execute(const CommandArgument &arg)
 {
+    auto && size = arg.Args.size();
+    if (size == 0 || size > 1)
+    {
+        this->_result = this->PrintHelp();
+        return false;
+    }
+
+    this->_result = "SUCCESS!";
     return true;
 }
 
 string HelpCommand::GetResult()
 {
-    return "";
+    return this->_result;
 }
--- unit_test/help_command_test.hpp
+++ unit_test/help_command_test.hpp
@@ -34,4 +34,29 @@ TEST_F(HelpCommandTest, PrintHelp)
     EXPECT_STREQ(expect.c_str(), result.c_str());
 }
 
+TEST_F(HelpCommandTest, NoArg)
+{
+    CommandArgument arg;
+    string expect = "Shows help info about command, i.e. .help <command>";
+
+    EXPECT_FALSE(sut->Execute(arg));
+    
+    auto result = sut->GetResult();
+    EXPECT_STREQ(expect.c_str(), result.c_str());
+}
+
+TEST_F(HelpCommandTest, TooManyArgs)
+{
+    CommandArgument arg;
+    arg.Args.push_back("one");
+    arg.Args.push_back("two");
+
+    string expect = "Shows help info about command, i.e. .help <command>";
+
+    EXPECT_FALSE(sut->Execute(arg));
+    
+    auto result = sut->GetResult();
+    EXPECT_STREQ(expect.c_str(), result.c_str());
+}
+
 #endif
