59fc5e568c0932f13ee48d06abf62deffdb30c35
sebszczec@wp.pl <sebszczec@wp.pl>
2018-02-04 16:27:34 +0100

    account manager fills data from DB

--- src/account/account_manager.cpp
+++ src/account/account_manager.cpp
@@ -10,8 +10,22 @@ namespace account
 {
     void AccountManager::RefreshWithDB()
     {
-        Logger::Log("AccountManager: refreshing user base with database");
+        Logger::Log("AccountManager: refreshing user base with DB");
         auto instance = Database::GetInstance();
+
+        if (!instance->Connect())
+        {
+            Logger::LogError("AccountManager: unable to connect to DB");
+            return;
+        }
+    
         auto result = instance->GetTable("users");
+        instance->Disconnect();
+
+        for (auto & row : result)
+        {
+            this->_userInDB[row["name"]].Username = row["name"];
+            this->_userInDB[row["name"]].Password = row["password"];
+        }
     }
 }
