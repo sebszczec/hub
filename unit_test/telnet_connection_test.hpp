b9536e639d597da2fef79f09d3e55b7aae7d5057
sebszczec@wp.pl <sebszczec@wp.pl>
2018-10-07 17:37:22 +0200

    enabling missing telnet UTs

--- unit_test/telnet_connection_test.hpp
+++ unit_test/telnet_connection_test.hpp
@@ -1,121 +1,105 @@
 #ifndef __TELNET_CONNECTION_TEST_HPP
 #define __TELNET_CONNECTION_TEST_HPP
 
-// #include <gtest/gtest.h>
-// #include "telnet_connection.hpp"
-// #include "system.hpp"
+#include <gtest/gtest.h>
+#include "telnet_connection.hpp"
+#include "tcp_connection_storage.hpp"
+#include "system.hpp"
+#include "icommand.hpp"
 
-// using namespace network;
+using namespace commands;
+using namespace network;
 
-// class OverridedTelnetConnection : public TelnetConnection
-// {
-// private:
-// public:
-//     OverridedTelnetConnection()
-//     : TelnetConnection(0, nullptr, nullptr)
-//     {
-//     }
+class TelnetConnectionTest : public testing::Test
+{
+protected:
+    boost::asio::io_service ios;
+    TcpServerConnectionStorage parent;
+    TelnetConnection * _sut;
+public:
+    void SetUp() override
+    {
+        this->_sut = new TelnetConnection(ios, parent);
+    }
 
-//     bool ExtractCommand(const string & message, string & command, CommandArgument & arg)
-//     {
-//         return TelnetConnection::ExtractCommand(message, command, arg);
-//     }
+    void TearDown() override
+    {
+        delete this->_sut;
+    }
+};
 
-//     void ExtractParameters(const string & message, CommandArgument & arg)
-//     {
-//         TelnetConnection::ExtractParameters(message, arg);
-//     }
-// };
+TEST_F(TelnetConnectionTest, ExtractNoCommand)
+{
+    string message = "", command = "";
+    CommandArgument arg;
 
+    EXPECT_FALSE(_sut->ExtractCommand(message, command, arg));
+}
 
-// class TelnetConnectionTest : public testing::Test
-// {
-// protected:
-//     OverridedTelnetConnection * _sut;
-// public:
-//     void SetUp() override
-//     {
-//         this->_sut = new OverridedTelnetConnection();
-//     }
+TEST_F(TelnetConnectionTest, CommandWithSpace)
+{
+    string message = "command parameter", command = "";
+    CommandArgument arg;
 
-//     void TearDown() override
-//     {
-//         delete this->_sut;
-//     }
-// };
+    EXPECT_TRUE(_sut->ExtractCommand(message, command, arg));
+    EXPECT_STREQ(string("command").c_str(), command.c_str());
+}
 
-// TEST_F(TelnetConnectionTest, ExtractNoCommand)
-// {
-//     string message = "", command = "";
-//     CommandArgument arg;
+TEST_F(TelnetConnectionTest, CommandWithEnter)
+{
+    string message = "command\rparameter", command = "";
+    CommandArgument arg;
 
-//     EXPECT_FALSE(_sut->ExtractCommand(message, command, arg));
-// }
+    EXPECT_TRUE(_sut->ExtractCommand(message, command, arg));
+    EXPECT_STREQ(string("command").c_str(), command.c_str());
+}
 
-// TEST_F(TelnetConnectionTest, CommandWithSpace)
-// {
-//     string message = "command parameter", command = "";
-//     CommandArgument arg;
+TEST_F(TelnetConnectionTest, EmptyParameters)
+{
+    string message = "";
+    CommandArgument arg;
 
-//     EXPECT_TRUE(_sut->ExtractCommand(message, command, arg));
-//     EXPECT_STREQ(string("command").c_str(), command.c_str());
-// }
+    _sut->ExtractParameters(message, arg);
 
-// TEST_F(TelnetConnectionTest, CommandWithEnter)
-// {
-//     string message = "command\rparameter", command = "";
-//     CommandArgument arg;
+    EXPECT_EQ(0, arg.Args.size());
+}
 
-//     EXPECT_TRUE(_sut->ExtractCommand(message, command, arg));
-//     EXPECT_STREQ(string("command").c_str(), command.c_str());
-// }
+TEST_F(TelnetConnectionTest, ExtractParameters)
+{
+    string message = "parameter1 parameter2 parameter3\r";
+    CommandArgument arg;
 
-// TEST_F(TelnetConnectionTest, EmptyParameters)
-// {
-//     string message = "";
-//     CommandArgument arg;
+    _sut->ExtractParameters(message, arg);
 
-//     _sut->ExtractParameters(message, arg);
+    ASSERT_EQ(3, arg.Args.size());
+    EXPECT_STREQ(string("parameter1").c_str(), arg.Args[0].c_str());
+    EXPECT_STREQ(string("parameter2").c_str(), arg.Args[1].c_str());
+    EXPECT_STREQ(string("parameter3").c_str(), arg.Args[2].c_str());
+}
 
-//     EXPECT_EQ(0, arg.Args.size());
-// }
+TEST_F(TelnetConnectionTest, ExtractParametersWithoutEnter)
+{
+    string message = "parameter1 parameter2 parameter3";
+    CommandArgument arg;
 
-// TEST_F(TelnetConnectionTest, ExtractParameters)
-// {
-//     string message = "parameter1 parameter2 parameter3\r";
-//     CommandArgument arg;
+    _sut->ExtractParameters(message, arg);
 
-//     _sut->ExtractParameters(message, arg);
+    ASSERT_EQ(2, arg.Args.size());
+    EXPECT_STREQ(string("parameter1").c_str(), arg.Args[0].c_str());
+    EXPECT_STREQ(string("parameter2").c_str(), arg.Args[1].c_str());
+}
 
-//     ASSERT_EQ(3, arg.Args.size());
-//     EXPECT_STREQ(string("parameter1").c_str(), arg.Args[0].c_str());
-//     EXPECT_STREQ(string("parameter2").c_str(), arg.Args[1].c_str());
-//     EXPECT_STREQ(string("parameter3").c_str(), arg.Args[2].c_str());
-// }
+TEST_F(TelnetConnectionTest, CommandWithParameters)
+{
+    string message = "command parameter1 parameter2\r", command = "";
+    CommandArgument arg;
 
-// TEST_F(TelnetConnectionTest, ExtractParametersWithoutEnter)
-// {
-//     string message = "parameter1 parameter2 parameter3";
-//     CommandArgument arg;
+    EXPECT_TRUE(_sut->ExtractCommand(message, command, arg));
+    EXPECT_STREQ(string("command").c_str(), command.c_str());
 
-//     _sut->ExtractParameters(message, arg);
-
-//     ASSERT_EQ(2, arg.Args.size());
-//     EXPECT_STREQ(string("parameter1").c_str(), arg.Args[0].c_str());
-//     EXPECT_STREQ(string("parameter2").c_str(), arg.Args[1].c_str());
-// }
-
-// TEST_F(TelnetConnectionTest, CommandWithParameters)
-// {
-//     string message = "command parameter1 parameter2\r", command = "";
-//     CommandArgument arg;
-
-//     EXPECT_TRUE(_sut->ExtractCommand(message, command, arg));
-//     EXPECT_STREQ(string("command").c_str(), command.c_str());
-
-//     ASSERT_EQ(2, arg.Args.size());
-//     EXPECT_STREQ(string("parameter1").c_str(), arg.Args[0].c_str());
-//     EXPECT_STREQ(string("parameter2").c_str(), arg.Args[1].c_str());
-// }
+    ASSERT_EQ(2, arg.Args.size());
+    EXPECT_STREQ(string("parameter1").c_str(), arg.Args[0].c_str());
+    EXPECT_STREQ(string("parameter2").c_str(), arg.Args[1].c_str());
+}
 
 #endif
