12d8316d88fe5bfac8cedaac8e7cf0817fb2ef98
Seba <sebszczec@wp.pl>
2018-10-21 15:00:02 +0200

    new UTs added

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "logger.hpp"
+#include "signal_handler.hpp"
 #include "access_level_test.hpp"
 #include "account_manager_test.hpp"
 #include "command_manager_test.hpp"
@@ -15,9 +16,11 @@
 #include "system_test.hpp"
 #include "tcp_connection_storage_test.hpp"
 #include "telnet_connection_test.hpp"
+#include "timer_test.hpp"
 #include "uptime_command_test.hpp"
 #include "user_test.hpp"
 #include "whoami_command_test.hpp"
+#include "worker_test.hpp"
 
 using machine::Logger;
 
@@ -26,6 +29,7 @@ int main(int ac, char* av[])
   testing::InitGoogleTest(&ac, av);
 
   machine::System::InitializeMembersForUT();
+  machine::SignalHandler::RegisterExitSignals();
   auto result = RUN_ALL_TESTS();
   machine::System::FreeMembersForUT();
 
